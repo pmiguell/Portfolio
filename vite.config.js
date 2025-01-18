cd9fdf5e4a92bb339d185a18c419e29bf37f993b
Pedro Miguel <pedromiguellitoo@gmail.com>
2025-01-17 22:27:44 -0300

    fixing curriculum vitae path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/portfolio/',
+  assetsInclude: ['**/*.docx'],
   plugins: [react()],
   server: {
     host: true
